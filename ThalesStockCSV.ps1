91f6251c3630c231365423b0f3bcfcc667bfc370
James Laurence <jameslaurence82@gmail.com>
2024-01-12 10:10:14 -0400

    updated the unix timestamp for the url to be a variable

--- ThalesStockCSV.ps1
+++ ThalesStockCSV.ps1
@@ -1,8 +1,8 @@
 # param([string]$DT)
-
+$DT = '1705017599'
 # Write-Host("The date passed is: " + $DT)
-# period 1 978307200 corresponds to January 1, 2001.
-# period 2 1704424699 corresponds to January 5, 2024.
+# period 1 978307200 corresponds to 2000-01-01
+# period 2 1705017599 corresponds to 2021-01-11
 
 ########################################
 ######## Thales Stock Prices: ##########
@@ -12,10 +12,10 @@
 #########################################################
 
 # Thales France Stock Prices (HO.PA) >>>>> EURO!!!<<<<<<<
-Write-Host "https://query1.finance.yahoo.com/v7/finance/download/HO.PA?period1=978307200&period2=1704424699&interval=1d&events=history&includeAdjustedClose=true"
+Write-Host "https://query1.finance.yahoo.com/v7/finance/download/HO.PA?period1=978307200&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
 
 # Fetch the data
-$response1 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/HO.PA?period1=978307200&period2=1704424699&interval=1d&events=history&includeAdjustedClose=true"
+$response1 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/HO.PA?period1=978307200&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
 
 # Convert the response content from a CSV string to objects
 $THdata = $response1.Content | ConvertFrom-Csv
@@ -32,10 +32,10 @@ $THdata | Export-Csv -Path "E:/5-Data Analytics Winter 2024/DBAS3090 - Applied D
 
 # France Stock Market Indicator (^FCHI) >>>>> EURO!!!<<<<<<<
 
-Write-Host "https://query1.finance.yahoo.com/v7/finance/download/%5EFCHI?period1=978307200&period2=1704424699&interval=1d&events=history&includeAdjustedClose=true"
+Write-Host "https://query1.finance.yahoo.com/v7/finance/download/%5EFCHI?period1=978307200&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
 
 # Fetch the data
-$response2 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/%5EFCHI?period1=978307200&period2=1704424699&interval=1d&events=history&includeAdjustedClose=true"
+$response2 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/%5EFCHI?period1=978307200&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
 
 # Convert the response content from a CSV string to objects
 $CACdata = $response2.Content | ConvertFrom-Csv
@@ -48,10 +48,10 @@ $CACdata | Export-Csv -Path "E:/5-Data Analytics Winter 2024/DBAS3090 - Applied 
 # Yahoo Finances uses UNIX timestamp in the URL for dates
 #########################################################
 
-Write-Host "https://query1.finance.yahoo.com/v7/finance/download/%5ESPX?period1=978307200&period2=1704424699&interval=1d&events=history&includeAdjustedClose=true"
+Write-Host "https://query1.finance.yahoo.com/v7/finance/download/%5ESPX?period1=978307200&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
 
 # Fetch the data
-$response3 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/%5ESPX?period1=978307200&period2=1704424699&interval=1d&events=history&includeAdjustedClose=true"
+$response3 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/%5ESPX?period1=978307200&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
 
 # Convert the response content from a CSV string to objects
 $SPdata = $response3.Content | ConvertFrom-Csv
@@ -62,12 +62,12 @@ $SPdata | Export-Csv -Path "E:/5-Data Analytics Winter 2024/DBAS3090 - Applied D
 # Yahoo Finances uses UNIX timestamp in the URL for dates
 #########################################################
 
-# Euro Stoxx 50 Index >>>>> EURO!!!<<<<<<<
+# Euro Stoxx 50 eIndex >>>>> EURO!!!<<<<<<<
 
-Write-Host "https://query1.finance.yahoo.com/v7/finance/download/%5EGDAXI?period1=978307200&period2=1704424699&interval=1d&events=history&includeAdjustedClose=true"
+Write-Host "https://query1.finance.yahoo.com/v7/finance/download/%5EGDAXI?period1=978307200&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
 
 # Fetch the data
-$response4 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/%5EGDAXI?period1=978307200&period2=1704424699&interval=1d&events=history&includeAdjustedClose=true"
+$response4 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/%5EGDAXI?period1=978307200&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
 
 # Convert the response content from a CSV string to objects
 $daxdata = $response4.Content | ConvertFrom-Csv
