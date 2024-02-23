8a49fbd332d4285e30423dd58852ee3a1aa1dc45
James Laurence <jameslaurence82@gmail.com>
2024-02-23 09:26:38 -0400

    updated python file to be run in SSIS

---
+++ lastdateasUNIX.sql
@@ -0,0 +1,18 @@
+SELECT DATEDIFF(second, '1970-01-01', COALESCE(MinDate, '2000-01-01')) as LastDT
+FROM(
+    SELECT MIN(dt) as MinDate
+    FROM
+    (
+        SELECT COALESCE(MAX([FK_DT_Date]), '2000-01-01') as dt
+        FROM [dbo].[THALES_STOCK]
+        UNION
+        SELECT COALESCE(MAX([FK_DT_Date]), '2000-01-01') as dt
+        FROM [dbo].[S_P_INDEX]
+        UNION
+        SELECT COALESCE(MAX([FK_DT_Date]), '2000-01-01') as dt
+        FROM [dbo].[EURO_INDEX]
+        UNION	
+        SELECT COALESCE(MAX([FK_DT_Date]), '2000-01-01') as dt
+        FROM [dbo].[FRANCE_INDEX]
+    ) AS x
+) AS y
