397cc08715c31fd45563d8ef0049f9116c80b722
James Laurence <jameslaurence82@gmail.com>
2024-03-01 13:36:51 -0400

    visualstudio automation and updated .py and .ps1 scripts

--- ThalesStockCSV.ps1
+++ ThalesStockCSV.ps1
@@ -1,21 +1,31 @@
-# param([string]$DT)
-$DT = '1707955200'
-# Write-Host("The date passed is: " + $DT)
-# period 1 946684800 corresponds to 2000-01-01
-# period 2 1708042110 corresponds to 2024-02-27
+# get min date value from SSIS expression
+param([string]$DT)
+Write-Host("The UNIX date passed is: " + $DT)
 
-########################################
-######## Thales Stock Prices: ##########
-########################################
+# Get today's date in Unix Epoch
+$todayDate = [math]::Round((Get-Date).ToUniversalTime().Subtract((Get-Date "1970-01-01")).TotalSeconds)
 
-# Yahoo Finances uses UNIX timestamp in the URL for dates
-#########################################################
+# Print the Unix epoch
+Write-Host "Today's date in Unix epoch: $todayDate"
+
+# sleep time to ensure python script can complete before dataflow task begins
+Start-Sleep -Seconds 3
+
+# period 1 corresponds to min date
+# period 2 corresponds to todays date
+
+# ########################################
+# ######## Thales Stock Prices: ##########
+# ########################################
+
+# # Yahoo Finances uses UNIX timestamp in the URL for dates
+# #########################################################
 
 # Thales France Stock Prices (HO.PA) >>>>> EURO!!!<<<<<<<
-Write-Host "https://query1.finance.yahoo.com/v7/finance/download/HO.PA?period1=946684800&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
+Write-Host "https://query1.finance.yahoo.com/v7/finance/download/HO.PA?period1=$DT&period2=$todayDate&interval=1d&events=history&includeAdjustedClose=true"
 
 # Fetch the data
-$response1 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/HO.PA?period1=946684800&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
+$response1 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/HO.PA?period1=$DT&period2=$todayDate&interval=1d&events=history&includeAdjustedClose=true"
 
 # Convert the response content from a CSV string to objects
 $THdata = $response1.Content | ConvertFrom-Csv
@@ -32,10 +42,10 @@ $THdata | Export-Csv -Path "E:/5-Data Analytics Winter 2024/DBAS3090 - Applied D
 
 # France Stock Market Indicator (^FCHI) >>>>> EURO!!!<<<<<<<
 
-Write-Host "https://query1.finance.yahoo.com/v7/finance/download/%5EFCHI?period1=946684800&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
+Write-Host "https://query1.finance.yahoo.com/v7/finance/download/%5EFCHI?period1=$DT&period2=$todayDate&interval=1d&events=history&includeAdjustedClose=true"
 
 # Fetch the data
-$response2 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/%5EFCHI?period1=946684800&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
+$response2 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/%5EFCHI?period1=$DT&period2=$todayDate&interval=1d&events=history&includeAdjustedClose=true"
 
 # Convert the response content from a CSV string to objects
 $CACdata = $response2.Content | ConvertFrom-Csv
@@ -48,10 +58,10 @@ $CACdata | Export-Csv -Path "E:/5-Data Analytics Winter 2024/DBAS3090 - Applied 
 # Yahoo Finances uses UNIX timestamp in the URL for dates
 #########################################################
 
-Write-Host "https://query1.finance.yahoo.com/v7/finance/download/%5ESPX?period1=946684800&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
+Write-Host "https://query1.finance.yahoo.com/v7/finance/download/%5ESPX?period1=$DT&period2=$todayDate&interval=1d&events=history&includeAdjustedClose=true"
 
 # Fetch the data
-$response3 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/%5ESPX?period1=946684800&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
+$response3 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/%5ESPX?period1=$DT&period2=$todayDate&interval=1d&events=history&includeAdjustedClose=true"
 
 # Convert the response content from a CSV string to objects
 $SPdata = $response3.Content | ConvertFrom-Csv
@@ -64,17 +74,19 @@ $SPdata | Export-Csv -Path "E:/5-Data Analytics Winter 2024/DBAS3090 - Applied D
 
 # Euro Stoxx 50 eIndex >>>>> EURO!!!<<<<<<<
 
-Write-Host "https://query1.finance.yahoo.com/v7/finance/download/%5EGDAXI?period1=946684800&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
+Write-Host "https://query1.finance.yahoo.com/v7/finance/download/%5EGDAXI?period1=$DT&period2=$todayDate&interval=1d&events=history&includeAdjustedClose=true"
 
-# Fetch the data
-$response4 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/%5EGDAXI?period1=946684800&period2=$DT&interval=1d&events=history&includeAdjustedClose=true"
+# # Fetch the data
+$response4 = Invoke-WebRequest -Uri "https://query1.finance.yahoo.com/v7/finance/download/%5EGDAXI?period1=$DT&period2=$todayDate&interval=1d&events=history&includeAdjustedClose=true"
 
-# Convert the response content from a CSV string to objects
+# # Convert the response content from a CSV string to objects
 $daxdata = $response4.Content | ConvertFrom-Csv
 
-# Export the data to a CSV file
+# # Export the data to a CSV file
 $daxdata | Export-Csv -Path "E:/5-Data Analytics Winter 2024/DBAS3090 - Applied Data Analytics/Project/dirtyEURO_Index.csv" -NoTypeInformation
 
+# # run python script after csv files are downloaded
+python "E:/5-Data Analytics Winter 2024/DBAS3090 - Applied Data Analytics/Project/dropnull.py"
 
-# run python script after csv files are downloaded
-python .\dropnull.py
+# sleep time to ensure python script can complete before dataflow task begins
+Start-Sleep -Seconds 5
--- dropnull.py
+++ dropnull.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # load the 4 dirty csvs from yahoo finance into dataframes
-thales = pd.read_csv('dirtyThales_Stock.csv')
-france = pd.read_csv('dirtyFRA_Index.csv')
-sp500 = pd.read_csv('dirtySP500_INDEX.csv')
-euro = pd.read_csv('dirtyEURO_Index.csv')
+thales = pd.read_csv("E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\Project\\dirtyThales_Stock.csv")
+france = pd.read_csv("E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\Project\\dirtyFRA_Index.csv")
+sp500 = pd.read_csv("E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\Project\\dirtySP500_INDEX.csv")
+euro = pd.read_csv("E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\Project\\dirtyEURO_Index.csv")
 
 # remove nulls from dirty dataframes
 clean_thales = thales.dropna()
@@ -12,8 +12,8 @@ clean_france = france.dropna()
 clean_sp500 = sp500.dropna()
 clean_euro = euro.dropna()
 
-# export clean dataframes to become clean csv's without panda's index column
-clean_thales.to_csv("clean_thales.csv", index=False)
-clean_france.to_csv("clean_france.csv", index=False)
-clean_sp500.to_csv("clean_sp500.csv", index=False)
-clean_euro.to_csv("clean_euro.csv", index=False)
+# export clean dataframes to become clean csv"s without panda"s index column
+clean_thales.to_csv("E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\Project\\clean_thales.csv", index=False)
+clean_france.to_csv("E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\Project\\clean_france.csv", index=False)
+clean_sp500.to_csv("E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\Project\\clean_sp500.csv", index=False)
+clean_euro.to_csv("E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\Project\\clean_euro.csv", index=False)
