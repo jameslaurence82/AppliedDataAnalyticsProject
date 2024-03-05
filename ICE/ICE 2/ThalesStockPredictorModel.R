d44fff340b64cee03893466322b2b97f1357347b
James Laurence <jameslaurence82@gmail.com>
2024-03-05 18:10:13 -0400

    produced correlations in R and python

--- ICE/ICE 2/ThalesStockPredictorModel.R
+++ ICE/ICE 2/ThalesStockPredictorModel.R
@@ -2,7 +2,7 @@ library(RODBC)
 library(caret)
 library(dplyr)
 library(corrgram)
-
+library(fields)
 
 ######################################################################################
 # connect to sql server DB thalesstockpredictor to export vw_COMBINED_MODEL view
@@ -26,18 +26,17 @@ Model_Data <- sqlQuery(dbconnection, query1)
 odbcClose(dbconnection)
 
 ######################################################################################
-# # Remove the first Row (slice(-1)) of Data because of y Value and reset index
+# Prep Model_Data DF for correlation, normalization, Train/Test/Validate split
 ######################################################################################
-Model_Data <- as.data.frame(Model_Data %>% slice(-1)) # NextDay_Close Null removed
 
 # View the sliced Model_Data Dataframe
 View(Model_Data)
 
 # output to csv before ML steps
-# write.csv(Model_Data, file = "E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\ICE\\ICE 2\\Model_Norm.csv", row.names = FALSE)
+# write.csv(Model_Data, file = "E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\project\\ICE\\ICE 2\\Model_Norm.csv", row.names = FALSE)
 
-# Work with a copy of the original Model_Date dataframe
-Model_Norm <- as.data.frame(Model_Data) 
+# copy Model_Data and remove the first Row (slice(-1)), reset index as DF to Model_Norm
+Model_Norm <- as.data.frame(Model_Data %>% slice(-1)) # NextDay_Close Null has entire row removed
 
 ######################################################################################
 # Correlation Matrix
@@ -48,10 +47,10 @@ df_numeric <- Model_Norm[sapply(Model_Norm, is.numeric)]
 # copy df_numeric for null removal
 df_naomit <- as.data.frame(df_numeric)
 
-# Calculate correlation with NA
-correlation_matrix <- cor(df_numeric)
-# View correlation with THA_NextDay_Close with NA
-correlation_with_target <- correlation_matrix[, "THA_NextDay_Close"]
+# # Calculate correlation with NA -----------> This Correlation Matrix is full of Nulls
+# correlation_matrix <- cor(df_numeric)
+# # View correlation with THA_NextDay_Close with NA
+# correlation_with_target <- correlation_matrix[, "THA_NextDay_Close"]
 
 # Calculate correlation - ignore NA
 correlation_matrixNA <- cor(df_numeric, use = "complete.obs")  # ignores NA values
@@ -59,12 +58,54 @@ correlation_matrixNA <- cor(df_numeric, use = "complete.obs")  # ignores NA valu
 correlation_with_targetNA <- correlation_matrixNA[, "THA_NextDay_Close"]
 
 # Calculate correlation - Omit NA
-correlation_matrixOmitOmitNA <- cor(df_naomit)  # ignores NA values
+correlation_matrixOmitNA <- cor(df_naomit)  # ignores NA values
 # View correlation with THA_NextDay_Close - ignore NA
 correlation_with_targetOmitNA <- correlation_matrixOmitNA[, "THA_NextDay_Close"]
 
-# Create correlation matrix
-corrgram(df_numeric, order=TRUE, color.legend=TRUE)
+# Start the png device with specified file path, name, and size
+png("E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\project\\ICE\\ICE 2\\corr_ignoreNA.png", width = 2000, height = 1024)
+
+# Create the corrgram
+corrgram(df_numeric, order=TRUE, lower.panel=panel.shade,
+         upper.panel=panel.pie, text.panel=panel.txt, main="Correlation With Nulls Ignored")
+
+# Add a color legend using image.plot
+image.plot(legend.only=TRUE, col=colorRampPalette(c("blue", "white", "red"))(100), zlim=c(-1,1))
+
+# Close the png device
+dev.off()
+
+# Start the png device with specified file path, name, and size
+png("E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\project\\ICE\\ICE 2\\corr_OmitNA.png", width = 2000, height = 1024)
+
+# Create the corrgram
+corrgram(df_naomit, order=TRUE, lower.panel=panel.shade,
+         upper.panel=panel.pie, text.panel=panel.txt, main="Correlation With Nulls Omitted")
+
+# Add a color legend using image.plot
+image.plot(legend.only=TRUE, col=colorRampPalette(c("blue", "white", "red"))(100), zlim=c(-1,1))
+
+# Close the png device
+dev.off()
+
+# Start the png device with specified file path, name, and size
+png("E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\project\\ICE\\ICE 2\\corr_barOmitNA.png", width = 2000, height = 1024)
+
+# Create the bar plot with a title
+barplot(correlation_with_targetOmitNA, main = "Correlation With Nulls Omitted to THA_NextDay_Close", horiz = TRUE, las = 1, cex.names=0.7)
+
+# Close the png device
+dev.off()
+
+
+# Start the png device with specified file path, name, and size
+png("E:\\5-Data Analytics Winter 2024\\DBAS3090 - Applied Data Analytics\\project\\ICE\\ICE 2\\corr_barignoreNA.png", width = 2000, height = 1024)
+
+# Create the bar plot with a title
+barplot(correlation_with_targetNA, main = "Correlation With Nulls Ignored to THA_NextDay_Close", horiz = TRUE, las = 1, cex.names=0.7)
+
+# Close the png device
+dev.off()
 
 ######################################################################################
 # Split dataframe into Training, Validation, Testing before normalization
